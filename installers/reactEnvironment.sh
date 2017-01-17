19f9e4836f2c7e1ed7129a030139d2582e2cdce4
robtweed <rtweed@mgateway.com>
2017-01-17 17:17:35 +0000

    Build 2.2.1 - added React environment installation script

---
+++ installers/reactEnvironment.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+# Set up a QEWD / React.js development environment
+
+cd ~/qewd
+
+npm install react react-dom babelify babel-preset-react react-bootstrap react-toastr react-select socket.io-client
+npm install jquery ewd-client ewd-react-tools ewd-xpress-react
+
+npm install -g browserify
+npm install -g uglify-js
+
+cd ~/qewd/www/qewd-monitor
+npm install babel-preset-es2015
+
+# Now you can compile an application bundle:
+#  cp ~/qewd/node_modules/qewd-monitor/www/*.js ~/qewd/www/qewd-monitor
+#  cd ~/qewd/www/qewd-monitor
+#  or
+#  cd ~/qewd/node_modules/qewd-monitor/www
+#  browserify -t [ babelify --compact false --presets [es2015 react] ] app.js | uglifyjs > bundle.js
+#  cp ~/qewd/node_modules/qewd-monitor/www/bundle.js ~/qewd/www/qewd-monitor
