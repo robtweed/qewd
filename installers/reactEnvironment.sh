436677cfc590a0839be4dea1b8301b1f5987517f
robtweed <rtweed@mgateway.com>
2018-09-24 09:59:59 +0100

    Build 2.38.1: Updated the optional React Development Environment installation file

--- installers/reactEnvironment.sh
+++ installers/reactEnvironment.sh
@@ -2,21 +2,23 @@
 
 # Set up a QEWD / React.js development environment
 
+# 24 September 2018
+
 cd ~/qewd
 
-npm install react react-dom babelify babel-preset-react react-bootstrap react-toastr react-select socket.io-client
-npm install jquery ewd-client ewd-react-tools qewd-react
+npm install react react-dom babelify react-bootstrap react-toastr react-select socket.io-client
+npm install @babel/core @babel/preset-react @babel/preset-env
+npm install jquery ewd-client ewd-react-tools ewd-xpress-react qewd-react create-react-class
 
 npm install -g browserify
 npm install -g uglify-js
-
-cd ~/qewd/www/qewd-monitor
-npm install babel-preset-es2015
 
 # Now you can compile an application bundle:
 #  cp ~/qewd/node_modules/qewd-monitor/www/*.js ~/qewd/www/qewd-monitor
 #  cd ~/qewd/www/qewd-monitor
 #  or
 #  cd ~/qewd/node_modules/qewd-monitor/www
-#  browserify -t [ babelify --compact false --presets [es2015 react] ] app.js | uglifyjs > bundle.js
+
+#  browserify -o bundle.js -t [ babelify --presets [ @babel/preset-env @babel/preset-react ] --plugins [ @babel/plugin-transform-classes ] ] app.js 
+
 #  cp ~/qewd/node_modules/qewd-monitor/www/bundle.js ~/qewd/www/qewd-monitor
